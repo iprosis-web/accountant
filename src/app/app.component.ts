71c0c20843ec226e6308ba5c449c04e85a85e742
AminMhana1997 <mhana.amin@gmail.com>
2020-03-03 13:22:38 +0200

    added customersreports filter function which returns all reports/filtered reports, added get all statuses function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReportsService } from './services/reports.service';
+import { DateFilterModel } from './models/dateFilterModel';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AccountantApp';
+  
+  constructor(private reportService: ReportsService){
+    
+  }
+
+  ngOnInit(){
+    let datefilter: DateFilterModel = { startDate: null, endDate : null };
+    
+    let test = this.reportService.getCustomersReports(datefilter,"1",null);
+    console.log(test);
+  }
 }
--- src/app/services/reports.service.ts
+++ src/app/services/reports.service.ts
@@ -3,6 +3,7 @@ import { Subject } from 'rxjs';
 import { CustomerReportModel } from '../models/customerReportModel';
 import { DateFilterModel } from '../models/dateFilterModel';
 import { customer } from '../models/customer';
+import { reports } from '../models/report';
 
 @Injectable({
   providedIn: 'root'
@@ -27,16 +28,89 @@ export class ReportsService {
   }
   ];
 
+  private reports: reports[] = [{
+    id: 1,
+    customerId: "1",
+    reportDate: new Date,
+    creatDate: new Date,
+    status: "בעבודה",
+    indication: 1,
+    comment: "חסרים דיווחי משכורות"
+  },
+  {
+    id: 2,
+    customerId: "2",
+    reportDate: new Date,
+    creatDate: new Date,
+    status: "בעבודה",
+    indication: 1,
+    comment: "חסרים דיווחי משכורות"
+  },
+  {
+    id: 3,
+    customerId: "3",
+    reportDate: new Date,
+    creatDate: new Date,
+    status: "בעבודה",
+    indication: 1,
+    comment: "חסרים דיווחי משכורות"
+  },
+  {
+    id: 4,
+    customerId: "1",
+    reportDate: new Date,
+    creatDate: new Date,
+    status: "בחופש",
+    indication: 1,
+    comment: "חסרים דיווחי משכורות"
+  }
+  ];
+
+  statuses = ["בעבודה","הסתיים","לא הותחל"]
+
   constructor() { }
 
-  getCustomersReports(dateFilter: DateFilterModel, customerId: number, status: string){
+  getCustomersReports(dateFilter: DateFilterModel, customerId: string, status: string){
     //bring all reports
+    let filteredReports = this.reports.filter(report => 
+      (report.customerId == customerId || customerId == undefined || customerId == null) &&
+      (dateFilter.startDate == null || dateFilter.startDate == undefined || (report.reportDate >= dateFilter.startDate && report.reportDate <= dateFilter.endDate))&&
+      (report.status == status || status == undefined || status == null));
     
+    return this.mapReportsToCustomerReports(filteredReports, this.getAllCustomers());
+  }
+
+  mapReportsToCustomerReports(reports: reports[], customers: customer[]){
+    let customersReports: CustomerReportModel[] = [];
+    if(reports && customers){
+      for(let report of reports){
+        let currentUser = customers.find(c => c.id == report.customerId);
+        if(currentUser){
+          customersReports.push({
+            reportID: report.id,
+            customerID: currentUser.id,
+            companyName: currentUser.companyName,
+            companyEmail: null,
+            date: report.reportDate,
+            status: report.status,
+            indication: report.indication,
+            comment: report.comment
+          });
+        }
+      }
+    }
+    return customersReports;
   }
 
   getAllCustomers(){
     if(this.customers){
       return this.customers.slice();
+    }
+  }
+
+  getAllStatuses(){
+    if(this.statuses){
+      return this.statuses.slice();
     }
   }
 }
