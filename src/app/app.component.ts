b022e25cc17ed256b38a0720ff070e7096b5280c
DanaEFalah <dana.e.falah@gmail.com>
2020-03-04 17:12:18 +0200

    add the sidebar to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Renderer } from '@angular/core';
 import { ReportsService } from './services/reports.service';
 import { DateFilterModel } from './models/dateFilterModel';
 
@@ -8,8 +8,16 @@ import { DateFilterModel } from './models/dateFilterModel';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AccountantApp';
-  
-  constructor(){
+items = ['מסך ניהולי', 'הזנת דיווח חודשי ללקוח' , 'מסך ניתוח ללקוח' , 'תחזוקת נתוני לקוח' ]
+hrefItems = ['reports' , 'monthly_customer_report' , 'customer_report' , 'maintenace_report']
+  constructor(private render: Renderer){
+  }
+
+  routeSidebar(event:any){
+    console.log(event.target);
+    
+    // event.preventDefault();
+    this.render.setElementClass(event.target, "active", true);
+
   }
 }
