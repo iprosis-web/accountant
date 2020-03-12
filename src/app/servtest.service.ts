bb145b5d81f505322f3f0cd6bc1cee69f4b19cd0
Evgeny <evgeny.grin@gmail.com>
2020-03-12 14:19:48 +0200

    CRUD completed in testcomp. usew router /testcomp to see

--- src/app/servtest.service.ts
+++ src/app/servtest.service.ts
@@ -7,15 +7,9 @@ import { AngularFirestore } from "@angular/fire/firestore";
 export class ServtestService {
   constructor(private firestore: AngularFirestore) {}
 
-  getData(id) {
-    return (
-      this.firestore
-        .collection("dataItems", ref => ref.where("id", "==", id))
-        //.collection("dataItems")
-        .get()
-    );
-  }
+  // CRUD:
 
+  //C: Create
   postData(data) {
     return new Promise<any>((resolve, reject) => {
       this.firestore
@@ -27,4 +21,61 @@ export class ServtestService {
         );
     });
   }
+
+  // R: Read
+  // 1. Example1 by data range where id1 = data.id, id2 = data.name
+  getData(data) {
+    console.log("Service get data:::", data);
+    let id1 = data.id;
+    let id2 = data.name;
+    return (
+      this.firestore
+        .collection("dataItems", ref => ref.where("id", ">", id1).where("id", "<", id2))
+        //.collection("dataItems")
+        .get()
+    );
+    }
+
+  // R: Read
+  // 2. Example1 by data range where id1 = data.id, id2 = data.name
+  getDataById(id) {
+    return (
+      this.firestore
+        .collection("dataItems", ref => ref.where("id", "==", id))
+        .get()
+    );
+    }
+
+
+    // U: Update
+    updateData(id ,data) {
+      return new Promise<any>((resolve, reject) => {
+        this.firestore
+          .collection("dataItems")
+          .doc(id)
+          .update(data)
+          .then(
+            res => {},
+            err => reject(err)
+          );
+      });
+    }
+
+    // D: delete
+    deleteData(id) {
+      return new Promise<any>((resolve, reject) => {
+        this.firestore
+          .collection("dataItems")
+          .doc(id)
+          .delete()
+          .then(
+            res => {},
+            err => reject(err)
+          );
+      });
+    }
+    
+
+    
+  
 }
