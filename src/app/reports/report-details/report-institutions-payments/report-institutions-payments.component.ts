465799075f58339bc1ed8fa789c94989db0967da
Sham762 <sham.sh622@gmail.com>
2020-03-15 14:48:55 +0200

    report institutions payment editing

--- src/app/reports/report-details/report-institutions-payments/report-institutions-payments.component.ts
+++ src/app/reports/report-details/report-institutions-payments/report-institutions-payments.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportInstitutionsPaymentsComponent implements OnInit {
 
+  taxableTurnoverA: number;
+  taxableCapitalTurnoverB: number;
+  generalVATTurnoverI: number;
+  exemptionTurnoverC: number;
+  exemptionCapitalTurnoverD: number;
+  generalExemptionTurnoverJ: number;
+  advanceTurnoverK: number;
+  fInputTaxF: number;
+  retentionTaxG:number;
+  percentageOfAdvancesH: number;
+  paypalVATN: number;
+  calculatedDownPaymentL: number;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getGeneralVAT() {
+    this.generalVATTurnoverI = this.taxableTurnoverA + this.taxableCapitalTurnoverB;
+    this.generalExemptionTurnoverJ = this.exemptionTurnoverC + this.exemptionCapitalTurnoverD;
+    this.advanceTurnoverK = this.taxableTurnoverA + this.exemptionTurnoverC;
+    this.paypalVATN = ((17/100) * this.taxableTurnoverA-(this.fInputTaxF+this.retentionTaxG));
+    this.calculatedDownPaymentL = (this.percentageOfAdvancesH * this.advanceTurnoverK - this.retentionTaxG);
+  }
+
+  onSaveMethod() {
+    
   }
 
 }
